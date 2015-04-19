33c6c5c10bd4dd6a56956a0633d3b52809f10c72
Catherine Johnson <cfjohnson@earthlink.net>
2015-04-19 15:09:18 -0600

    added basic checkout functions for adding, declaring product class, creating product list in constructor, initialized before this, and created test for processing coupon discount code.

--- CheckOut/Checkout.cs
+++ CheckOut/Checkout.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
 
 namespace CheckOut
 {
 	public class Checkout
 	{
+		private IList<Product> _products;
+
 		public Checkout ()
 		{
-			
+			_products = new List<Product>();
+
 		}
 		public int getTotal() {
 			var total = 0;
-
+			foreach (var i in _products) {
+				//total += _products.Price;
+				total += i.Price;
+			}
 			return total;
 		}
+		public void addProduct(Product product) {
+			_products.Add(product);
+		}
+
+		public decimal processCouponDiscount(int total, int percentOff) {
+			
+			decimal percentageOff = (percentOff/100m);
+				
+			var	discount = (total * percentageOff);
+			var	totalAfterDiscount = Math.Round(total - discount, 0);
+
+			return totalAfterDiscount;
+		}
+	}
+	public class Product {
+		public string Name { get; set; }
+		public int Price { get; set; }
 	}
 }
 
--- CheckOut/Test.cs
+++ CheckOut/Test.cs
@@ -9,9 +9,41 @@ namespace CheckOut
 		[Test ()]
 		public void TestCase ()
 		{
-			var checkout = new Checkout ();
+			var checkout = new Checkout();
 			var total = checkout.getTotal ();
 			Assert.That(total, Is.EqualTo(0));
+		}
+		[Test ()]
+		public void Should_return_50_for_product_A  ()
+		{
+			var checkout = new Checkout();
+			var productA = new Product { Name = "A", Price = 50 };
+			checkout.addProduct(productA);
+			checkout.getTotal();
+			Assert.That (checkout.getTotal(), Is.EqualTo(50));
+		}
+		[Test ()]
+		public void Should_return_120_for_product_A_and_B ()
+		{
+			var checkout = new Checkout ();
+			var productA = new Product { Name = "A", Price = 20 };
+			var productB = new Product { Name = "B", Price = 100 };
+			checkout.addProduct(productA);
+			checkout.addProduct(productB);
+			checkout.getTotal();
+			Assert.That (checkout.getTotal(), Is.EqualTo(120));
+		}
+		[Test ()]
+		public void Should_return_80_for_product_A_and_B_after_coupon_code ()
+		{
+			var checkout = new Checkout ();
+			var productA = new Product { Name = "A", Price = 70 };
+			var productB = new Product { Name = "B", Price = 30 };
+			checkout.addProduct(productA);
+			checkout.addProduct(productB);
+			var initialTotal = checkout.getTotal();
+			checkout.processCouponDiscount (initialTotal, 20);
+			Assert.That (checkout.processCouponDiscount(initialTotal, 20), Is.EqualTo(80));
 		}
 	}
 }
