d3afc577d6fa36cbe0bcc66dbf0687cedb522cdf
Catherine Johnson <cfjohnson@earthlink.net>
2015-04-26 17:26:00 -0600

    started discount class to process discounts.

---
+++ CheckOut/Discount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckOut
+{
+	public class Discount
+	{
+		
+		public Discount ()
+		{
+			
+		}
+
+		public void Check_For_Discount(IList<Product> _products)
+		{
+			var products = _products;
+			return;
+		}
+
+		public int Triple_A_Discount ()
+		{
+			return 1;
+		}
+
+		public int Double_B_Discount ()
+		{
+			return 2;
+		}
+	}
+}
+
