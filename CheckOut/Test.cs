a749dba35de3773a012b067f3821290d28e85fec
Catherine Johnson <cfjohnson@earthlink.net>
2015-04-26 18:10:39 -0600

    started writing out tests.

--- CheckOut/Test.cs
+++ CheckOut/Test.cs
@@ -26,29 +26,38 @@ namespace CheckOut
 		public void Should_return_120_for_product_A_and_B ()
 		{
 			var checkout = new Checkout ();
-			var productA = new Product { Name = "A", Price = 20 };
-			var productB = new Product { Name = "B", Price = 100 };
-			checkout.addProduct(productA);
-			checkout.addProduct(productB);
-			checkout.getTotal();
-			Assert.That (checkout.getTotal(), Is.EqualTo(120));
-		}
-		[Test ()]
-		public void Should_process_A_discount ()
-		{
-			var checkout = new Checkout ();
 			var productA = new Product { Name = "A", Price = 50 };
 			var productB = new Product { Name = "B", Price = 30 };
 			checkout.addProduct(productA);
+			checkout.addProduct(productB);
+			checkout.getTotal();
+			Assert.That (checkout.getTotal(), Is.EqualTo(80));
+		}
+		[Test ()]
+		public void Should_return_130_for_3_A_Products ()
+		{
+			var checkout = new Checkout ();
+			var productA = new Product { Name = "A", Price = 50 };
 			checkout.addProduct(productA);
 			checkout.addProduct(productA);
-			checkout.addProduct(productB);
-			checkout.addProduct(productB);
-			checkout.addProduct(productB);
-			checkout.addProduct(productB);
-			checkout.addProduct(productB);
+			checkout.addProduct(productA);
 			var total = checkout.getTotal();
 			var totalAfterDiscounts = checkout.ApplyDiscounts(total);
+			Assert.That (totalAfterDiscounts, Is.EqualTo(130));
+		}
+
+		[Test ()]
+		public void Should_return_180_for_4_A_Products ()
+		{
+			var checkout = new Checkout ();
+			var productA = new Product { Name = "A", Price = 50 };
+			checkout.addProduct(productA);
+			checkout.addProduct(productA);
+			checkout.addProduct(productA);
+			checkout.addProduct(productA);
+			var total = checkout.getTotal();
+			var totalAfterDiscounts = checkout.ApplyDiscounts(total);
+			Assert.That (totalAfterDiscounts, Is.EqualTo(180));
 		}
 
 	}
